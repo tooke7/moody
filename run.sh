54763cdfa03597d572c7377b55e9d647cb967341
Jacob O'Bryant <foo@jacobobryant.com>
2017-04-13 19:43:57 +0000

    added documentation

---
+++ run.sh
@@ -0,0 +1,1 @@
+lein compile reco.reco && java -cp $(lein classpath):./target/base+system+user+dev/classes/ reco.reco
--- src/reco/reco.clj
+++ src/reco/reco.clj
@@ -121,38 +121,68 @@
 
 (defn -main [& args]
   (println "starting")
+
+  ; this initializes the user's library with the given songs. This information
+  ; actually isn't used very much right now because the recommender works with
+  ; songs given by calls to add_event.
   (let [rec (new reco.reco [{:artist "rise against" :album "myalbum" :title "the dirt whispered"}
                             {:artist "evanescence" :album "foobar" :title "lithium"}])]
-    (.add_event rec "a" "a" "a" true 2000)
-    (.add_event rec "b" "b" "b" true 2000)
-    (.add_event rec "c" "c" "c" true 2000)
-    (.add_event rec "d" "d" "d" true 2000)
-    (.add_event rec "e" "e" "e" true 2000)
-    (.add_event rec "f" "f" "f" true 2000)
+    
+    ; explanation of the first line in session 0:
+    ; artist: a
+    ; album: a
+    ; title: a
+    ; skipped: true (i.e. the user skipped the song instead of listening to it).
+    ; time: 2000 (i.e. this song ended 2000 seconds after the unix epoch). If
+    ; there's at least 1800 seconds (half an hour) between songs, then we count
+    ; that as a divider between sessions.
 
+    ; session 0
+    (.add_event rec "a" "a" "a" true 2000)
+    (.add_event rec "b" "b" "b" false 2000)
+    (.add_event rec "c" "c" "c" false 2000)
+    (.add_event rec "d" "d" "d" false 2000)
+    (.add_event rec "e" "e" "e" false 2000)
+    (.add_event rec "f" "f" "f" false 2000)
+
+    ; session 1
     (.add_event rec "a" "a" "a" false 4000)
     (.add_event rec "b" "b" "b" false 4000)
     (.add_event rec "c" "c" "c" false 4000)
-    (.new_session rec)
     (.add_event rec "d" "d" "d" false 4000)
     (.add_event rec "e" "e" "e" false 4000)
     (.add_event rec "g" "g" "g" false 4000)
 
-    (.add_event rec "a" "a" "a" true 6000)
-    (.add_event rec "b" "b" "b" true 6000)
-    (.add_event rec "c" "c" "c" true 6000)
-    (.add_event rec "d" "d" "d" true 6000)
+    ; this will choose "f" because it's the only song in the first session that
+    ; isn't in the current session
+    (println (str "next song: " (.pick_next rec)))
+
+    ; session 2 (current session)
+    (.add_event rec "a" "a" "a" false 6000)
+    (.add_event rec "b" "b" "b" false 6000)
+    (.add_event rec "c" "c" "c" false 6000)
+    (.add_event rec "d" "d" "d" false 6000)
+
+    ; if you uncomment these lines, the system won't be able to generate any
+    ; recommendations because all the songs it knows about are already in the
+    ; current session. Instead, it'll choose a random song from the library
+    ; (i.e. either "the dirt whispered" or "lithium").
     ;(.add_event rec "e" "e" "e" false 6000)
     ;(.add_event rec "f" "f" "f" false 6000)
     ;(.add_event rec "g" "g" "g" false 6000)
 
-    (let [sessions (:sessions @@rec)]
-      (println (count sessions))
-      ;(println (.pick_next rec))
-      ;(println (.pick_next rec))
-      ;(println (.pick_next rec))
-      ;(println (.pick_next rec))
-      ;(println (.pick_next rec))
-      ;(println (.pick_next rec))
-      ;(println (.pick_next rec)))))
-      )))
+    ; these recommendations will be either e, f or g.
+    ; e has the highest probability of being picked because it's in both
+    ; previous sessions.
+    ; g is next because sessions 1 and 2 are more similar than sessions 0 and 2.
+    ; f is the least likely prediction.
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+    (println (.pick_next rec))
+))
