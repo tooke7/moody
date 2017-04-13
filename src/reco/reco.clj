5799642015ac75ad7790ea4c08306a771b6f383d
Jacob O'Bryant <foo@jacobobryant.com>
2017-04-13 19:20:21 +0000

    add new session command

--- src/reco/reco.clj
+++ src/reco/reco.clj
@@ -6,6 +6,7 @@
              [add_event [String, String, String, boolean] void]
              [add_event [java.util.Map, boolean, long] void]
              [pick_next [] java.util.Map]
+             [new_session [] void]
              [testing [] String]]
    :init init
    :constructors {[java.util.Collection] []}))
@@ -23,37 +24,52 @@
              :sessions nil
              :last-time 0})])
 
+(defn -new_session [this]
+  (swap! (.state this)
+         (fn [state]
+           (assoc state
+                  :sessions (cons nil (:sessions state))))))
+
 (defn ses-append [sessions mdata skipped create-session]
   (if create-session
-    (cons {mdata skipped} sessions)
-    (if (contains? (first sessions) mdata)
-      sessions
-      (cons (assoc (first sessions) mdata skipped)
-            (rest sessions)))))
+    (do
+      ;(println "NEW")
+      (cons {mdata skipped} sessions)
+      )
+    (do
+      ;(println "CURRENT")
+      (if (contains? (first sessions) mdata)
+        sessions
+        (cons (assoc (first sessions) mdata skipped)
+              (rest sessions))))))
 
 (defn -add_event
   ([this, mdata, skipped, timestamp]
-     (swap! (.state this)
-            (fn [state]
-              (assoc state :last-time timestamp
-                     :sessions (ses-append (:sessions state)
-                                           mdata
-                                           skipped
-                                           (> timestamp (+ (:last-time state) ses-threshold)))))))
+   ;(println (str "add_event: " mdata ", " skipped ", " timestamp))
+   (swap! (.state this)
+          (fn [state]
+            (assoc state :last-time timestamp
+                   :sessions (ses-append (:sessions state)
+                                         mdata
+                                         skipped
+                                         (> timestamp (+ (:last-time state) ses-threshold)))))))
   ([this, artist, album, title, skipped, timestamp]
-   (.add_event this {:artist artist :album album :title title} skipped timestamp))
+   (.add_event this {"artist" artist "album" album "title" title} skipped timestamp))
   ([this, artist, album, title, skipped]
    (.add_event this artist album title skipped (now))))
 
 (defn dbg [desc arg]
-  (println (str desc ": " arg))
+  ;(println (str desc ": " arg))
   arg)
 
 (defn dist [sesa sesb]
   (let [universe (intersection (set (keys sesa)) (set (keys sesb)))
         hits (filter #(not (and (sesa %1) (sesb %1))) universe)
         matches (map #(if (= (sesa %1) (sesb %1) false) 1 0) universe)]
-    (/ (reduce + matches) (count hits))))
+    ;(/ (reduce + matches) (count hits))))
+    (if (zero? (count hits))
+      0
+      (/ (reduce + matches) (count hits)))))
 
 (defn aggregate [sessions]
   (let [universe (apply union (map #(set (:recs %1)) sessions))]
@@ -71,6 +87,7 @@
   random spin of a roulette wheel with compartments proportional to
   slices."
   [slices]
+  (dbg "slices" slices)
   (let [total (reduce + slices)
         r (rand total)]
     (loop [i 0 sum 0]
@@ -86,51 +103,56 @@
 
 (defn -pick_next [this]
   (let [sessions (:sessions @(.state this))
-        neighbors (filter #(:recs %1)
+        neighbors (filter #(and (not-empty (:recs %1))
+                                (not (zero? (:score %1))))
                           (map #(create-neighbor (first sessions) %1)
                                (rest sessions)))
         nearest (take k (sort-by :score neighbors))
         candidates (aggregate nearest)]
+    ;(println (str "candidates: " candidates))
     (if (empty? candidates)
       (rand-nth (:songs @(.state this)))
-      (:mdata (nth candidates (wrand (vec (map :score candidates))))))))
-
-(defn -testing [this] "hello there")
+      (dbg "choice" (:mdata (nth candidates (wrand (vec (map :score candidates)))))))))
 
 (defn -deref [this]
   (.state this))
+
+(defn -testing [this] "hello there")
 
 (defn -main [& args]
   (println "starting")
   (let [rec (new reco.reco [{:artist "rise against" :album "myalbum" :title "the dirt whispered"}
                             {:artist "evanescence" :album "foobar" :title "lithium"}])]
     (.add_event rec "a" "a" "a" true 2000)
-    (.add_event rec "b" "b" "b" false 2000)
-    (.add_event rec "c" "c" "c" false 2000)
-    (.add_event rec "d" "d" "d" false 2000)
-    (.add_event rec "e" "e" "e" false 2000)
-    (.add_event rec "f" "f" "f" false 2000)
+    (.add_event rec "b" "b" "b" true 2000)
+    (.add_event rec "c" "c" "c" true 2000)
+    (.add_event rec "d" "d" "d" true 2000)
+    (.add_event rec "e" "e" "e" true 2000)
+    (.add_event rec "f" "f" "f" true 2000)
 
     (.add_event rec "a" "a" "a" false 4000)
     (.add_event rec "b" "b" "b" false 4000)
     (.add_event rec "c" "c" "c" false 4000)
+    (.new_session rec)
     (.add_event rec "d" "d" "d" false 4000)
     (.add_event rec "e" "e" "e" false 4000)
     (.add_event rec "g" "g" "g" false 4000)
 
-    (.add_event rec "a" "a" "a" false 6000)
-    (.add_event rec "b" "b" "b" false 6000)
-    (.add_event rec "c" "c" "c" false 6000)
-    (.add_event rec "d" "d" "d" false 6000)
-    (.add_event rec "e" "e" "e" false 6000)
-    (.add_event rec "f" "f" "f" false 6000)
-    (.add_event rec "g" "g" "g" false 6000)
+    (.add_event rec "a" "a" "a" true 6000)
+    (.add_event rec "b" "b" "b" true 6000)
+    (.add_event rec "c" "c" "c" true 6000)
+    (.add_event rec "d" "d" "d" true 6000)
+    ;(.add_event rec "e" "e" "e" false 6000)
+    ;(.add_event rec "f" "f" "f" false 6000)
+    ;(.add_event rec "g" "g" "g" false 6000)
 
     (let [sessions (:sessions @@rec)]
-      (println (.pick_next rec))
-      (println (.pick_next rec))
-      (println (.pick_next rec))
-      (println (.pick_next rec))
-      (println (.pick_next rec))
-      (println (.pick_next rec))
-      (println (.pick_next rec)))))
+      (println (count sessions))
+      ;(println (.pick_next rec))
+      ;(println (.pick_next rec))
+      ;(println (.pick_next rec))
+      ;(println (.pick_next rec))
+      ;(println (.pick_next rec))
+      ;(println (.pick_next rec))
+      ;(println (.pick_next rec)))))
+      )))
